42a7c6fce315c7ca3416f02aee64b94fe681bed6
Valdir Bruxel Junior <hagnat@gmail.com>
2013-11-16 02:43:08 -0200

    - created TileBean - TileBeans have vertical and horizonal position - created ScenarioException

---
+++ lib/beans/TileBean.class.php
@@ -0,0 +1,62 @@
+<?php
+
+class TileBean extends BaseBean
+{
+  protected $horizontal_position;
+  
+  protected $vertical_position;
+  
+  public function __construct($horizontal, $vertical)
+  {
+    $this
+      ->setHorizontalPosition($horizontal)
+      ->setVerticalPosition($vertical)
+    ;
+  }
+  
+  /**
+   * @param integer $position
+   * @throws ScenarioException for negative values for position
+   * @return TileBean self
+   */
+  public function setHorizontalPosition($position)
+  {
+    if (0 > (integer)$position) {
+      throw new ScenarioException ("Tile horizontal position must be a positive value");
+    }
+    $this->horizontal_position = (integer)$position;
+    
+    return $this;
+  }
+  
+  /**
+   * @return integer
+   */
+  public function getHorizontalPosition()
+  {
+    return $this->horizontal_position;
+  }
+  
+  /**
+   * @param integer $position
+   * @throws ScenarioException for negative values for position
+   * @return TileBean self
+   */
+  public function setVerticalPosition($position)
+  {
+    if (0 > (integer)$position) {
+      throw new ScenarioException ("Tile vertical position must be a positive value");
+    }
+    $this->vertical_position = (integer)$position;
+    
+    return $this;
+  }
+  
+  /**
+   * @return integer
+   */
+  public function getVerticalPosition()
+  {
+    return $this->vertical_position;
+  }
+}
---
+++ lib/exceptions/ScenarioException.class.php
@@ -0,0 +1,5 @@
+<?php
+
+class ScenarioException extends Exception
+{
+}
